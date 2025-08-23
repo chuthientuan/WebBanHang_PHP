b6137d0987a081c56d48d32eeb0e357960123f17
chuthientuan <tuanchu24112004@gmail.com>
2025-08-23 16:40:38 +0700

    Giao dien trang admin-8

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/trang-chu', [HomeController::class, 'index']);
+
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', [AdminController::class, 'show_dashboard']);
