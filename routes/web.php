c16ee3761e96f1b5e39af9bb263cfc9b526ef99e
k59ta <K59tay04@gmail.com>
2025-09-09 01:25:01 +0700

    hetbai14

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
@@ -11,3 +12,12 @@ Route::get('/admin', [AdminController::class, 'index']);
 Route::get('/dashboard', [AdminController::class, 'show_dashboard']);
 Route::post('/admin-dashboard', [AdminController::class, 'dashboard']);
 Route::get('/logOut', [AdminController::class, 'logOut']);
+
+// category product
+Route::get('/add-category-product', [CategoryProductController::class, 'add_category_product']);
+Route::get('/all-category-product', [CategoryProductController::class, 'all_category_product']);
+
+Route::get('/unactive-category-product/{category_product_id} ', [CategoryProductController::class, 'unactive_category_product']);
+Route::get('/active-category-product/{category_product_id} ', [CategoryProductController::class, 'active_category_product']);
+
+Route::post('/save-category-product', [CategoryProductController::class, 'save_category_product']);
